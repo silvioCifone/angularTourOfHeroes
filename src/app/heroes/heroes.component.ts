965f61ed88eef8ed6caa7b7981e2176b86fa214d
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-24 15:11:03 +0100

    Add Heroes Visualization and Styling
    In the main page, the single hero visualization is changed in a list of
    heroes, with a particular styling for every element

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../interfaces/hero";
+import { HEROES } from "../some-data/mock-heroes";
 
 @Component({
   selector: "app-heroes",
@@ -7,10 +8,7 @@ import { Hero } from "../interfaces/hero";
   styleUrls: ["./heroes.component.css"],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm",
-  };
+  heroes: Hero[] = HEROES;
 
   constructor() {}
 
