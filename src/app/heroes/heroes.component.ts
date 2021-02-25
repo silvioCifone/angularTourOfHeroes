71ae5a280bad87fa1eddb45b959e5b9124124aa8
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-25 15:41:15 +0100

    Add Link to HeroDetailComponent
    It permit to navigate from a new screen that contains hero details, and
    where you can change hero name and go back to the page from where you
    come from

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ import { MessageService } from "../message.service";
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(
     private heroService: HeroService,
     private messageService: MessageService
@@ -20,11 +18,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
