9c9ddf97486faee3fcb7861528766a3e73fa9661
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-25 12:39:14 +0100

    Add Action Messages
    Everytime user click a hero or refresh the page, a new message is
    written and appear in the bottom of the app. All of this is managed
    by the MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Hero } from "./interfaces/hero";
+import { MessageService } from "./message.service";
 import { HEROES } from "./some-data/mock-heroes";
 
 @Injectable({
   providedIn: "root",
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add("HeroService: fetched heroes");
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HeroService } from "../hero.service";
 import { Hero } from "../interfaces/hero";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: "app-heroes",
@@ -10,9 +11,12 @@ import { Hero } from "../interfaces/hero";
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero?: Hero;
+  selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,11 +24,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
-    this.heroService
-      .getHeroes()
-      .subscribe((heroes) => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
